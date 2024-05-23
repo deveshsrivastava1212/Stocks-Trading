cc421ae7663add00887be6a08ece53fcd2ecf075
deveshsrivastava1212 <devesh.srivastava.1212@gmail.com>
2024-05-23 20:59:54 +0530

    Delete new frontend folder

--- frontend/stock-ui/src/axios/apis.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const API = axios.create({
-    baseURL :  "https://localhost:5000"
-})
-
-export const RegisterUser = (data) => API.post("/api/users", data)
-export const LoginUser = (data) => API.post("/api/auth", data)
-
